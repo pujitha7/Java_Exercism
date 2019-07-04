b916aa4e8c0837492ee723319f58dc04215ca15f
Pujitha.E <pujitha98.e@gmail.com>
2019-07-04 15:12:30 +0530

    Add files via upload

---
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class RnaTranscription {
+    private static HashMap<Character , Character> map = new HashMap<>() {{
+        put('G','C');
+        put('C','G');
+        put('T','A');
+        put('A','U');
+    }};
+    String transcribe(String dnaStrand) {
+        StringBuilder rnaStrand=new StringBuilder("");
+        for(int i=0;i<dnaStrand.length();i++){
+            rnaStrand.append(map.get(dnaStrand.charAt(i)));
+        }
+        return rnaStrand.toString();
+    }
+
+
+
+}
