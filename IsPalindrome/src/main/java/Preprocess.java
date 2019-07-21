17262ff1c9010c7614e8e8a56273fd74e1ff7ebf
Pujitha.E <pujitha98.e@gmail.com>
2019-07-21 09:38:01 +0530

    3rd iteration

--- IsPalindrome/src/main/java/Preprocess.java
+++ IsPalindrome/src/main/java/Preprocess.java
@@ -1,17 +1,7 @@
-import static java.lang.Character.isLetterOrDigit;
-
 public class Preprocess{
 
     public String processSentence(String sentence){
-        StringBuilder modified=new StringBuilder("");
-        char currChar;
-        for(int index=0;index<sentence.length();index++){
-            currChar=sentence.charAt(index);
-            if(isLetterOrDigit(currChar)){
-                modified.append(currChar);
-            }
-        }
-        return modified.toString().toLowerCase();
+        return sentence.replaceAll("[^a-zA-Z0-9]","").toLowerCase(); 
     }
 
 }
